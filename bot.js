5715730b98befd2530c5788e299a78b5fb5f1236
Yousef Sultan <yousef.su.2000@gmail.com>
2017-12-14 18:59:50 +0200

    1.0.0 First stable

--- bot.js
+++ bot.js
@@ -2,9 +2,8 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 bot.on('message', m => {
-	if (`${m.author.username}#${m.author.discriminator}` === 'AyyBot#9665')
-		return;
-	console.log(`${m.author.username}#${m.author.discriminator}@${m.channel.guild.name}#${m.channel.name} ${m.content}`);
+	if (m.author.bot) return;
+	console.log(`${m.author.tag}@${m.channel.guild.name}#${m.channel.name} ${m.content}`);
 	const matches = m.content.match(/(ay{2,10})/);
 	if (matches)
 		m.channel.send('lma' + 'o'.repeat(matches[1].length - 2));
